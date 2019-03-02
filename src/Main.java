a6a0d17467b83bbc3e42fac8c4559ca1d84ebeaf
Kevin Wang <kevinwang0324@gmail.com>
2019-03-01 23:43:54 -0800

    tested loading data method

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@
  */
 public class Main {
 
-    
+    public static void main(String[] args) {
+        String data = Utils.readFileAsString("data\\2016_Presidential_Results.csv");
+        System.out.println(data);
+    }
 }
