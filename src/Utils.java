652f6430866fa33b0368d819e41126ae6a8b1aaf
Kevin Wang <kevinwang0324@gmail.com>
2019-03-01 23:41:46 -0800

    implemented readfileasstring

---
+++ src/Utils.java
@@ -0,0 +1,19 @@
+public class Utils {
+
+    public static String readFileAsString(String filepath) {
+        StringBuilder output = new StringBuilder();
+
+        try (Scanner scanner = new Scanner(new File(filepath))) {
+
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                output.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
+    }
+
+}
