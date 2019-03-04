dbfe1e64b12bf17c1a5090587694d1bd63a44d15
Kevin Wang <kevinwang0324@gmail.com>
2019-03-03 18:53:29 -0800

    added getters and setters

---
+++ src/ElectionResult.java
@@ -0,0 +1,89 @@
+public class ElectionResult {
+    private String votes_dem,votes_gop,total_votes,per_dem,per_gop,diff,per_point_diff,state_abbr,county_name,combined_fips;
+
+    public ElectionResult() {
+
+    }
+
+    public String getVotes_dem() {
+        return votes_dem;
+    }
+
+    public void setVotes_dem(String votes_dem) {
+        this.votes_dem = votes_dem;
+    }
+
+    public String getVotes_gop() {
+        return votes_gop;
+    }
+
+    public void setVotes_gop(String votes_gop) {
+        this.votes_gop = votes_gop;
+    }
+
+    public String getTotal_votes() {
+        return total_votes;
+    }
+
+    public void setTotal_votes(String total_votes) {
+        this.total_votes = total_votes;
+    }
+
+    public String getPer_dem() {
+        return per_dem;
+    }
+
+    public void setPer_dem(String per_dem) {
+        this.per_dem = per_dem;
+    }
+
+    public String getPer_gop() {
+        return per_gop;
+    }
+
+    public void setPer_gop(String per_gop) {
+        this.per_gop = per_gop;
+    }
+
+    public String getDiff() {
+        return diff;
+    }
+
+    public void setDiff(String diff) {
+        this.diff = diff;
+    }
+
+    public String getPer_point_diff() {
+        return per_point_diff;
+    }
+
+    public void setPer_point_diff(String per_point_diff) {
+        this.per_point_diff = per_point_diff;
+    }
+
+    public String getState_abbr() {
+        return state_abbr;
+    }
+
+    public void setState_abbr(String state_abbr) {
+        this.state_abbr = state_abbr;
+    }
+
+    public String getCounty_name() {
+        return county_name;
+    }
+
+    public void setCounty_name(String county_name) {
+        this.county_name = county_name;
+    }
+
+    public String getCombined_fips() {
+        return combined_fips;
+    }
+
+    public void setCombined_fips(String combined_fips) {
+        this.combined_fips = combined_fips;
+    }
+
+
+}
