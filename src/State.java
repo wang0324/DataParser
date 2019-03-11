95789b8fee89cd24cd76303db9c5da91f3898ee9
Kevin Wang <kevinwang0324@gmail.com>
2019-03-11 00:26:20 -0700

    Finish State object

--- src/State.java
+++ src/State.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class State {
+    private String name; //state initials or name
+    private List<County> counties; //list of counties
+
+    public State(String name) {
+        this.name = name;
+        counties = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String s) {
+        this.name = s;
+    }
+
+    public void addCounty(County c) {
+        counties.add(c);
+    }
+
+    public void removeCounty(County c) {
+        counties.remove(c);
+    }
+
 }
