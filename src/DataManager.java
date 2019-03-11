021b1cad95c753df18401de08b8998b2bbecacba
Kevin Wang <kevinwang0324@gmail.com>
2019-03-11 00:25:49 -0700

    Added method to datamanager

--- src/DataManager.java
+++ src/DataManager.java
@@ -1,2 +1,13 @@
+import java.util.List;
+
 public class DataManager {
+    private List<State> states;
+
+    public DataManager() {
+
+    }
+
+    private void addState(State s) {
+        states.add(s);
+    }
 }
